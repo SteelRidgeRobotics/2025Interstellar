e6aa0f53d884bb641811553666e79241a5f32ddc
loftyy-o <chimao.okafor123@gmail.com>
2025-12-05 21:16:49 -0800

    add sim support

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
@@ -13,12 +14,40 @@ public class IntakeIOSim implements IntakeIO {
     private final DCMotorSim intakeSim = 
         new DCMotorSim(
             LinearSystemId.createDCMotorSystem(
-                coralIntakeSIM, 0.01,Constants.IntakeConstants.GEAR_RATIO),
+                coralIntakeSIM, 0.01, Constants.IntakeConstants.GEAR_RATIO),
             coralIntakeSIM);
 
+    private final DCMotorSim algaeSim = 
+        new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(
+                algaeIntakeSIM, 0.01, Constants.IntakeConstants.GEAR_RATIO),
+            algaeIntakeSIM);
+
     private double intakeAppliedVolts = 0.0;
-    private boolean intakeIgnoreLimits = false;
 
+@Override
+public void updateInputs(IntakeIOInputs inputs) {
+    intakeSim.setInputVoltage(MathUtil.clamp(intakeAppliedVolts,-12,12));
+    intakeSim.update(0.02);
 
+    inputs.algaeIntakeConnected = true;
+    inputs.algaeIntakeVelocityRadsPerSec = algaeSim.getAngularVelocityRadPerSec();
+    inputs.algaeIntakeAppliedVoltage = intakeAppliedVolts;
+    inputs.algaeIntakeCurrentAmps = Math.abs(algaeSim.getCurrentDrawAmps());
+
+    inputs.coralIntakeConnected = true;
+    inputs.coralIntakeVelocityRadsPerSec = intakeSim.getAngularVelocityRadPerSec();
+    inputs.coralIntakeAppliedVoltage = intakeAppliedVolts;
+    inputs.coralIntakeCurrentAmps = Math.abs(intakeSim.getCurrentDrawAmps());
+
+    inputs.coral2IntakeConnected = true;
+    inputs.coral2IntakeVelocityRadsPerSec = intakeSim.getAngularVelocityRadPerSec();
+    inputs.coral2IntakeAppliedVoltage = intakeAppliedVolts;
+    inputs.coral2IntakeCurrentAmps = Math.abs(intakeSim.getCurrentDrawAmps());
     
+} 
+
+public void setIntakeOpenLoop(double output) {
+    intakeAppliedVolts = output;
+    }
 }
--- src/main/java/frc/robot/subsystems/pivot/Pivot.java
+++ src/main/java/frc/robot/subsystems/pivot/Pivot.java
@@ -4,10 +4,11 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicDutyCycle;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import frc.robot.Constants.PivotConstants;
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
 
 public class Pivot extends SubsystemBase {
 
@@ -15,28 +16,47 @@ public class Pivot extends SubsystemBase {
   private TalonFX followerMotor = new TalonFX(frc.robot.Constants.CanIDs.RIGHT_PIVOT_TALON);
   private MotionMagicDutyCycle motionMagicControl;
 
-  public Pivot() {
+  public enum State {
+    DEFAULT(PivotConstants.START_ANGLE),
+    SCORING(PivotConstants.SCORING_ANGLE),
+    INTAKE(PivotConstants.GROUND_INTAKE_ANGLE);
 
-    TalonFXConfiguration config = new TalonFXConfiguration();
-    config.Feedback.RotorToSensorRatio = Constants.PivotConstants.GEAR_RATIO;
-    config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-    config.Slot0 = Constants.PivotConstants.GAINS;
+    private final Rotation2d rotation;
 
-    config.MotionMagic.MotionMagicCruiseVelocity = Constants.PivotConstants.MM_CRUISE_VELOCITY;
-    config.MotionMagic.MotionMagicAcceleration = Constants.PivotConstants.MM_ACCELERATION;
-    config.MotionMagic.MotionMagicJerk = Constants.PivotConstants.MM_CRUISE_VELOCITY * 0.2;
+    State(double rotations) {
+      this.rotation = Rotation2d.fromRotations(rotations);
+    }
+  }
 
-    masterMotor.getConfigurator().apply(config);
-    followerMotor.getConfigurator().apply(config);
+  private final PivotIO io;
+  private final PivotIOInputsAutoLogged inputs = new PivotIOInputsAutoLogged();
 
-    Follower followerRequest = new Follower(frc.robot.Constants.CanIDs.LEFT_PIVOT_TALON, true);
-    followerMotor.setControl(followerRequest);
+  private State currentState = State.DEFAULT;
 
-    masterMotor.setPosition(0);
+  public Pivot(PivotIO io) {
+
+    this.io = io;
+    setName("Pivot");
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Pivot", inputs);
+  }
+
+  public void setState(State state) {
+    currentState = state;
+    io.setPosition(currentState.rotation);
+  }
+
+  @AutoLogOutput(key = "Pivot/State")
+  public State getState() {
+    return currentState;
   }
 
   public void setPosition(double setpoint) {
-    
+
     masterMotor.setControl(motionMagicControl.withPosition(setpoint));
   }
 }
