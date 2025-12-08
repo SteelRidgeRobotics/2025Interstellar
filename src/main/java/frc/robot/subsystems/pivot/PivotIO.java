e26ab2c93b1b1a77f09268179fb330686603cd7b
Vorpal1610 <katie.percy.thiel@gmail.com>
2025-12-07 17:42:44 -0800

    fixed the pivot subsystem file hopefully maybe

--- src/main/java/frc/robot/subsystems/pivot/PivotIO.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotIO.java
@@ -8,6 +8,7 @@ public interface PivotIO {
   @AutoLog
   class PivotIOInputs {
     public boolean pivotConnected = false;
+    public Rotation2d pivotAbsolutePosition = Rotation2d.kZero;
     public double positionRads = 0.0;
     public double velocityRads = 0.0;
     public double pivotAppliedVolts = 0.0;
